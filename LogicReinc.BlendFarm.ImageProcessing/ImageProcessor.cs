4de59941ebc2ca479b659ddab93306f3083f8d61
filebin <alex.mousse9@gmail.com>
2024-01-27 15:37:18 +0300

    move image logic to ImageProcessor assembly

---
+++ LogicReinc.BlendFarm.ImageProcessing/ImageProcessor.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using Avalonia.Media.Imaging;
+
+namespace LogicReinc.BlendFarm.ImageProcessing;
+
+public class ImageProcessor {
+    private RenderTargetBitmap bitmap;
+
+    public ImageProcessor(int width, int height) {
+        bitmap = new RenderTargetBitmap(new PixelSize(width, height));
+    }
+
+    public void DrawImage(byte[] imageData, int x, int y) {
+        var ctx = bitmap.CreateDrawingContext(null);
+
+        using (var ms = new MemoryStream(imageData)) {
+            Bitmap image = new Bitmap(ms);
+            var imageSize = image.PixelSize.ToSize(1.0);
+            ctx.DrawBitmap(
+                image.PlatformImpl,
+                opacity: 1.0,
+                sourceRect: new Rect(imageSize),
+                destRect: new Rect(new Point(x, y), imageSize));
+        }
+    }
+
+    public void Save(MemoryStream ms) {
+        bitmap.Save(ms);
+    }
+}
